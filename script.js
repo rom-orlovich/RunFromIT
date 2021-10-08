350f66115e5ce26a4874703e82a8f650e2fab6bb
vina188 <madman280797@gmail.com>
2021-10-08 15:36:38 +0300

    updated form

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const item = document.querySelector(".item");
 const con = document.querySelector(".container");
 const btnStart = document.querySelector(".btn-str");
 const btnReset = document.querySelector(".btn-reset");
+const body = document.querySelector("body");
 document.addEventListener("touchmove ", function (e) {
   console.log(e);
 });
@@ -49,11 +50,20 @@ function calBorderLeft(pos, box) {
 function calBorderRight(pos, box) {
   return pos + box.offsetWidth / 2;
 }
+//cal margin width and height  of the box reletive to the container
+function calMarginWidth() {
+  return con.offsetWidth - box1.offsetWidth;
+}
+
+function calMarginHeight() {
+  return con.offsetHeight - box1.offsetHeight;
+}
 
 //random a number
 function mathRandom(min, max) {
   return Math.trunc(min + Math.random() * (max - min));
 }
+
 //change color
 function changeColor() {
   box1.style.backgroundColor = `rgba(${mathRandom(0, 255)},${mathRandom(
@@ -137,11 +147,11 @@ function box2Run() {
 //move the the active player by keyboard
 function move(e) {
   if (!tf) {
-    console.log(box1RL, box1TD);
     if (e.key === "ArrowRight")
       if (box1RL < calMarginWidth() * 0.98)
         box1.style.transform = `translate(${(box1RL +=
           speedBox1)}px, ${box1TD}px) `;
+
     if (e.key === "ArrowLeft")
       if (box1RL > 2)
         box1.style.transform = `translate(${(box1RL -=
@@ -151,18 +161,95 @@ function move(e) {
       if (box1TD > 0)
         box1.style.transform = `translate(${box1RL}px, ${(box1TD -=
           speedBox1)}px) `;
+
     if (e.key === "ArrowDown")
       if (box1TD < calMarginHeight() * 0.98)
         box1.style.transform = `translate(${box1RL}px, ${(box1TD +=
           speedBox1)}px) `;
   }
 }
-function calMarginWidth() {
-  return con.offsetWidth - box1.offsetWidth;
-}
+let c = 0;
+function touchMove(e) {
+  let touch = e.touches[0];
+  console.log(e.touches[0]);
+  // let touch = e;
+  let touchX = Math.trunc(touch.clientX);
+  let touchY = Math.trunc(touch.clientY);
+  // let posL = box1RL;
+  // let posR = 0;
+  // let posU = box1TD;
+  // let posD = 0;
+  let posRLB = box1RL;
 
-function calMarginHeight() {
-  return con.offsetHeight - box1.offsetHeight;
+  let posUDB = box1TD;
+  console.log(`move: ${c++}`);
+  console.log(" ");
+  console.log("touchX:", touchX, "touchY:", touchY);
+  console.log(" ");
+  console.log("box1:", "box1RL:", box1RL, "box1TD", box1TD);
+
+  console.log(" ");
+  // console.log(
+  //   "currentPos",
+  //   "posL",
+  //   posL,
+  //   "posR",
+  //   posR,
+  //   "posU",
+  //   posU,
+  //   "posD",
+  //   posD
+  // );
+
+  // move Left
+  if (touchX < posRLB)
+    if (box1RL > 2) {
+      // posL = touchX;
+      posRLB = touchX;
+
+      box1.style.transform = `translate(${(box1RL -=
+        speedBox1)}px, ${box1TD}px) `;
+    }
+
+  // move right
+
+  if (touchX > posRLB)
+    if (box1RL < calMarginWidth() * 0.98) {
+      // posR = touchX;
+      posRLB = touchX;
+      box1.style.transform = `translate(${(box1RL +=
+        speedBox1)}px, ${box1TD}px) `;
+    }
+
+  //moveUp
+  if (touchY < posUDB)
+    if (box1TD > 0) {
+      // posU = touchY;
+      posUDB = touchY;
+      box1.style.transform = `translate(${box1RL}px, ${(box1TD -=
+        speedBox1)}px) `;
+    }
+
+  // move down
+  if (touchY > posUDB)
+    if (box1TD < calMarginHeight() * 0.98) {
+      // posD = touchY;
+      posUDB = touchY;
+      box1.style.transform = `translate(${box1RL}px, ${(box1TD +=
+        speedBox1)}px) `;
+    }
+  console.log(" ");
+  // console.log(
+  //   "currentPos",
+  //   "posL",
+  //   posL,
+  //   "posR",
+  //   posR,
+  //   "posU",
+  //   posU,
+  //   "posD",
+  //   posD
+  // );
 }
 
 //reset the game
@@ -188,9 +275,9 @@ function reset() {
     1,
     430
   )}px`;
-  box1.style.transform = `translate(${
-    (con.offsetWidth - box1.offsetWidth) / 2
-  }px, ${(con.offsetHeight - box1.offsetHeight) / 2}px) `;
+  box1.style.transform = `translate(${calMarginWidth() / 2}px, ${
+    calMarginHeight() / 2
+  }px) `;
   item.style.transform = `translate(${itemX}px, ${itemY}px) `;
   changeColor();
 }
@@ -215,6 +302,8 @@ function play() {
 //game actions
 btnStart.addEventListener("click", play);
 btnReset.addEventListener("click", reset);
+box1.addEventListener("touchmove", touchMove);
+// document.addEventListener("touchmove", touchMove);
 
 //checking:
 //   console.log(
@@ -247,3 +336,24 @@ btnReset.addEventListener("click", reset);
 //     box2X,
 //     box2Y
 //   );
+// console.log(
+//   "width:",
+//   con.offsetWidth,
+//   "height:",
+//   con.offsetHeight,
+//   "width:",
+//   body.offsetWidth,
+//   "height:",
+//   body.offsetHeight
+// );
+
+// console.log(
+//   "min-width:",
+//   body.offsetWidth - con.offsetWidth,
+//   "max-width",
+//   con.offsetWidth,
+//   "min-height:",
+//   calBorderBot(con.offsetHeight / 2, con),
+//   "max-height",
+//   body.offsetHeight / 2 - con.offsetHeight / 2
+// );
