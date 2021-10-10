aa904e1f1dce30060186a5ca13ebe0f9ff49c53e
Vina188 <87934536+Vina188@users.noreply.github.com>
2021-10-10 10:06:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -396,7 +396,7 @@ function reset() {
   box1.style.opacity = "1";
   item.style.transform = `translate(${itemX}px, ${itemY}px) `;
   item.textContent = `+${pointAdd}P`;
-Jostick.classList.add("remove_btn");
+jostick.classList.add("remove_btn");
   jostick.classList.add("hidden_btn");
   box2.classList.add("hidden");
   item.classList.add("hidden");
